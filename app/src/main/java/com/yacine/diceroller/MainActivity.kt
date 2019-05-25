19146b2018b09b610c293b024a99c6564b42be95
yacinebenkaidali <fy_benkaidali@esi.dz>
2019-05-25 04:04:42 +0100

    Setting up started Project

--- app/src/main/java/com/yacine/diceroller/MainActivity.kt
+++ app/src/main/java/com/yacine/diceroller/MainActivity.kt
@@ -1,43 +1,32 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.yacine.diceroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Creates an Activity that hosts all of the fragments in the app
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        roll_button.setOnClickListener {
-            rollDice()
-        }
-    }
-
-    private fun rollDice() {
-        val rand = Random().nextInt(6) + 1
-        val diceRollImage = when (rand) {
-            1 -> {
-                R.drawable.dice_1
-            }
-            2 -> {
-                R.drawable.dice_2
-            }
-            3 -> {
-                R.drawable.dice_3
-            }
-            4 -> {
-                R.drawable.dice_4
-            }
-            5 -> {
-                R.drawable.dice_5
-            }
-            else -> {
-                R.drawable.dice_6
-            }
-        }
-        dice_num.setImageResource(diceRollImage)
+        setContentView(R.layout.main_activity)
     }
 
 }
